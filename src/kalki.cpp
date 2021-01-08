d110f67827c901c27000dac3d066fe1b62f1b1a3
MrFish1604 <mmrfishc@gmail.com>
2021-01-08 19:36:59 +0100

    I forgot the power...

--- src/kalki.cpp
+++ src/kalki.cpp
@@ -83,6 +83,18 @@ double calcRPN(string expr, unsigned char* error)
 				else
 					{*error=1;}
 			}
+			else if (word == "^")
+			{
+				if(stack.size()>1)
+				{
+					double a = stack.back();
+					stack.pop_back();
+					double& b = stack.back();
+					b = pow(b, a);
+				}
+				else
+					{*error=1; cout << "RPN syntaxe error" << endl; break;}
+			}
 			else if (word == "cos")
 			{
 				if(stack.size()>0)
@@ -253,6 +265,19 @@ double calcRPN(string expr, unsigned char* error, bool verbose)
 					double& b = stack.back();
 					if(verbose) cout << b << '/' << a;
 					b = b / a;
+				}
+				else
+					{*error=1; cout << "RPN syntaxe error" << endl; break;}
+			}
+			else if (word == "^")
+			{
+				if(stack.size()>1)
+				{
+					double a = stack.back();
+					stack.pop_back();
+					double& b = stack.back();
+					if(verbose) cout << b << '/' << a;
+					b = pow(b, a);
 				}
 				else
 					{*error=1; cout << "RPN syntaxe error" << endl; break;}
